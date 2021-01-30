a9676ac4ecfaa1db103d4b344850671e473bf020
keironjerome <Keyjerome@gmail.com>
2021-01-30 14:54:13 -0500

    added swagger dependencies to pom.xml and created the SwaggerConfig to implement swagger documentation for vehicles api.

---
+++ vehicles-api/src/main/java/com/udacity/vehicles/config/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package com.udacity.vehicles.config;
+
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .useDefaultResponseMessages(true);
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo("Vehicles API",
+                "This API is the backend of a car Website. \n" +
+                        "it stores vehicle data and gets pricing and \n" +
+                        "location data from external sources.",
+                "1.0", "http://www.udacity.com",
+                new Contact("Keiron Jerome", "www.udacity.com", "KSJerome1@gmail.com"),
+                "License of Api", "http://www.udacity.com/license", Collections.emptyList()
+        );
+    }
+}
